824373a85b860da79e5daa85a9d9d59401c995bf
Norielle Cruz <mail@noriellecruz.com>
2020-12-05 15:47:34 +0800

    Updated

--- script/adopanel.sh
+++ script/adopanel.sh
@@ -3,6 +3,7 @@ sudo apt -y update
 sudo apt -y install php-cli php-curl zip
 mkdir adopanel
 mv run.php adopanel/
-rm adopanel.sh
+mv adopanel.sh adopanel/
 rm adopanel.zip
+(crontab -l 2>/dev/null || true; echo "@reboot nohup php /home/pi/adopanel/run.php '$1' '$2' '$3' &") | crontab -
 echo "AdoPanel has been installed!"
